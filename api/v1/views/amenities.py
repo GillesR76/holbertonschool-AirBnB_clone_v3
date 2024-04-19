92eb0bd82eb840b35e70b9b09eabc1d63573b368
GillesR76 <gilles-richard76@hotmail.com>
2024-04-19 18:10:03 +0200

    pycodestyle corrections

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -15,7 +15,8 @@ def all_amenities():
     return jsonify(list_amenities)
 
 
-@app_views.route('/amenities/<amenity_id>/', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>/',
+                 methods=['GET'], strict_slashes=False)
 def amenity_object(amenity_id):
     """select amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
@@ -24,7 +25,8 @@ def amenity_object(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """delete amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
@@ -49,7 +51,8 @@ def create_amenity():
     return make_response(new_obj.to_dict(), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """update amenity by id"""
     new = request.get_json(silent=True)
