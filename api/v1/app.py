3818224fec78e73227902bbf434d5b714b41ab17
GillesR76 <gilles-richard76@hotmail.com>
2024-04-19 20:08:56 +0200

    Add CORS support to Flask application in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,24 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
+"""CORS() is a function provided by the Flask_CORS extension
+* app: applies the CORS policy to the entire app instance
+of the Flask application
+* ressources: dictionary that defines the CORS policy:
+    - r'/*': expression that matches all routes in the application
+    - 'origins':'0.0.0.0': sub dictionary that specifies the policy
+    for the matched routes: the application will accept 
+    cross-origin requests from host 0.0.0.0
+The whole line allows cross-origin requests from 0.0.0.0 
+to all routes in the Flask application.
+"""
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
