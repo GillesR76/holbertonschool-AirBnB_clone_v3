575775c531f17415f02714ef7e5a66d45183c860
GillesR76 <gilles-richard76@hotmail.com>
2024-04-19 18:08:01 +0200

    Refactor user routes in users.py to use make_response in return statements

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -5,7 +5,7 @@
 from models.user import User
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 
 
 @app_views.route('/users',
@@ -45,16 +45,17 @@ def delete_user(user_id):
                  strict_slashes=False)
 def create_user():
     """create a new user object"""
-    request_data = request.get_json()
+    request_data = request.get_json(silent=True)
     if request_data is None:
-        abort(400, 'Not a JSON')
+        return abort(400, 'Not a JSON')
     if 'email' not in request_data:
-        abort(400, 'Missing email')
+        return abort(400, 'Missing email')
     if 'password' not in request_data:
-        abort(400, 'Missing password')
-    new_user = User(**request_data)
-    new_user.save()
-    return jsonify(new_user.to_dict()), 201
+        return abort(400, 'Missing password')
+    new_user = User(name=request_data['name'])
+    storage.new(new_user)
+    storage.save()
+    return make_response(new_user.to_dict()), 201
 
 
 @app_views.route('/users/<user_id>', methods=["PUT"],
@@ -64,11 +65,11 @@ def update_user(user_id):
     user_update = storage.get(User, user_id)
     if user_update is None:
         abort(404)
-    request_data = request.get_json()
+    request_data = request.get_json(silent=True)
     if request_data is None:
         abort(400, 'Not a JSON')
     for key, value in request_data.items():
         if key not in ['id', 'email', 'created_at', 'updated_at']:
             setattr(user_update, key, value)
     user_update.save()
-    return jsonify(user_update.to_dict()), 200
+    return make_response(jsonify(user_update.to_dict()), 200)
