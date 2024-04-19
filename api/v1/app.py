3eba5dc7c679f93f80c84e6b937b55900b1cdd1a
GillesR76 <gilles-richard76@hotmail.com>
2024-04-19 20:11:15 +0200

    pycodestyle correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,9 +16,9 @@ of the Flask application
 * ressources: dictionary that defines the CORS policy:
     - r'/*': expression that matches all routes in the application
     - 'origins':'0.0.0.0': sub dictionary that specifies the policy
-    for the matched routes: the application will accept 
+    for the matched routes: the application will accept
     cross-origin requests from host 0.0.0.0
-The whole line allows cross-origin requests from 0.0.0.0 
+The whole line allows cross-origin requests from 0.0.0.0
 to all routes in the Flask application.
 """
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
